f8fc8dfe7097edcf65d1646ac9b20198ba79cf53
bateman001 <49342979+bateman001@users.noreply.github.com>
2024-03-08 10:22:40 -0500

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 function App() {
-    return <div></div>;
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    );
 }
 
 export default App;
